acad1b371041985c975eecc7727d66b5226293db
WangariM <wangariminyu@gmail.com>
2022-03-16 00:15:02 +0400

    Write a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,9 @@
 
 /**
  * main - Prints the alphabet in lowercase.
- * description - prints alphabet in lowercase
+ *
+ * Description: prints alphabet in lowercase
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
