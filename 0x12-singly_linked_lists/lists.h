170751737832ea8a29d63494e96ad99f4c822384
WangariM <wangariminyu@gmail.com>
2022-04-21 23:53:20 +0400

    Linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,35 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
-/**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
+/*
+ * File: lists.h
+ *
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
  */
 
-void free_list(list_t *head)
-{
-	list_t *current;
+#include <stdlib.h>
 
-	while ((current = head) != NULL)
-	{
-		head = head->next;
-		free(current->str);
-		free(current);
-	}
-}
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LISTS_H */
