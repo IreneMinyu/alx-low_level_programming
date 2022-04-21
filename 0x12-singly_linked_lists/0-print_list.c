c347cd106a4c282192555adb11de105254c6c2ba
WangariM <wangariminyu@gmail.com>
2022-04-21 23:46:57 +0400

    Print a list of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,25 @@
 #include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
 
+/**
+ * print_list - Prints a list of nodes
+ * @h: List of nodes
+ *
+ * Return: Number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	int count = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+
+	return (count);
 }
