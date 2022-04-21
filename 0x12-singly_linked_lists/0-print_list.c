4da3b20cd442cff0c0c964e2d56bf75460b74dd2
WangariM <wangariminyu@gmail.com>
2022-04-21 23:00:15 +0400

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,33 @@
 #include "lists.h"
+#include <stdio.h>
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * print_list - imrpime todos los elementos de size_t
+ * @h: puntero al inicio de la lista
+ * Return: retorna el numero de nodos
  */
 
 size_t print_list(const list_t *h)
 {
- size_t nelem;
+ size_t counter = 0;
 
- nelem = 0;
+ /*usamos un while para obtener el numero nodosen "counter"*/
+
  while (h != NULL)
  {
   if (h->str == NULL)
-   printf("[%d] %s\n", 0, "(nil)");
+  {
+   printf("[0] (nil)\n");
+   /*procedemos a imprimir los elementos*/
+   /*dirigimos el pointer a cada elemento de de la estructura a imprimir*/
+  }
   else
-   printf("[%d] %s\n", h->len, h->str);
+  {
+   printf("[%u] %s\n", h->len, h->str);
+  }
+
+  counter++;
   h = h->next;
-  nelem++;
  }
- return (nelem);
+
+ return (counter); /*retornamos la cantidad de nodos que se representan en i*/
 }
