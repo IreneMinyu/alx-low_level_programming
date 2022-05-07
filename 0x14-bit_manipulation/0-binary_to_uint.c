d41ad8d55b43d5246f44a26173e43f11b88a190d
WangariM <wangariminyu@gmail.com>
2022-05-07 12:40:06 +0400

    Executable file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number to an unsigned int.
+ * binary_to_unit - converts a binary,
+ * number to an unsigned int.
  * @b : pointing to a string of 0 and 1 chars.
  *
  * Return: the converted number or NULL if not 0 or 1.
